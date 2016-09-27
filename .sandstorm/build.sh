054e0a39e822728bc432c42885747df7b59b5ddb
Jason Paryani <github@jparyani.com>
2016-09-27 12:02:13 -0700

    Fix build

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -5,9 +5,10 @@ set -euo pipefail
 
 cd /opt/app/mediawiki-core
 
+rm -f /opt/app/mediawiki-core/composer.lock
 if [ -f /opt/app/mediawiki-core/composer.json ] ; then
     if [ ! -f composer.phar ] ; then
         curl -sS https://getcomposer.org/installer | php
     fi
-    php composer.phar install
+    php composer.phar install --no-dev
 fi
