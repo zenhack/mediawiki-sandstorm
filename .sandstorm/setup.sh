22905cadd6addaca5dc3c446cb27bae92e414807
Ian Denhardt <ian@zenhack.net>
2022-03-14 14:36:41 -0400

    First steps attempting to upgrade.
    - Vagrant-spk upgradevm
    - Pulled in some logic from the newer lemp stack, minus mysql.
    
    This doesn't build, I think because the current version of composer is
    too new for this old version of mediawiki. I plan on trying to upgrade
    it anyway.

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -1,97 +1,44 @@
 #!/bin/bash
 
+# When you change this file, you must take manual action. Read this doc:
+# - https://docs.sandstorm.io/en/latest/vagrant-spk/customizing/#setupsh
+
 set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
+
 apt-get update
-apt-get install -y nginx php5-fpm php5-sqlite php5-cli php5-curl git php5-dev php-apc imagemagick
-unlink /etc/nginx/sites-enabled/default
-cat > /etc/nginx/sites-available/sandstorm-php <<EOF
-server {
-    listen 8000 default_server;
-    listen [::]:8000 default_server ipv6only=on;
-
-    server_name localhost;
-    root /opt/app/mediawiki-core;
-    
-    # increase upload defaults
-    client_max_body_size 1000M;
-    client_header_timeout 30m;
-    client_body_timeout 30m;
-
-    index index.php;
-
-	  gzip off;
-
-    location ~ \\.php\$ {
-        fastcgi_pass unix:/var/run/php5-fpm.sock;
-        fastcgi_index index.php;
-        fastcgi_split_path_info ^(.+\\.php)(/.+)\$;
-        fastcgi_param  SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-        include fastcgi_params;
-    }
-
-    location / {
-        try_files \$uri \$uri/ @rewrite;
-    }
-
-    location @rewrite {
-        rewrite ^/(.*)$ /index.php?title=\$1&\$args;
-    }
-
-    location ^~ /maintenance/ {
-        return 403;
-    }
-
-    location ~* \.(js|css|png|jpg|jpeg|gif|ico)$ {
-        try_files \$uri /index.php;
-        expires max;
-        log_not_found off;
-    }
-
-    location = /_.gif {
-        expires max;
-        empty_gif;
-    }
-
-    location ^~ /cache/ {
-        deny all;
-    }
-}
-EOF
-ln -s /etc/nginx/sites-available/sandstorm-php /etc/nginx/sites-enabled/sandstorm-php
+apt-get install -y \
+	nginx \
+	php-fpm \
+	php-cli \
+	php-curl \
+	git \
+	php-dev \
+	php-mbstring \
+	php-intl \
+	php-sqlite3
 service nginx stop
-service php5-fpm stop
+service php7.3-fpm stop
 systemctl disable nginx
-systemctl disable php5-fpm
-# patch /etc/php5/fpm/pool.d/www.conf to not change uid/gid to www-data
+systemctl disable php7.3-fpm
+# patch /etc/php/7.3/fpm/pool.d/www.conf to not change uid/gid to www-data
 sed --in-place='' \
-        --expression='s/^listen.owner = www-data/#listen.owner = www-data/' \
-        --expression='s/^listen.group = www-data/#listen.group = www-data/' \
-        --expression='s/^user = www-data/#user = www-data/' \
-        --expression='s/^group = www-data/#group = www-data/' \
-        /etc/php5/fpm/pool.d/www.conf
-# patch /etc/php5/fpm/php-fpm.conf to not have a pidfile
+        --expression='s/^listen.owner = www-data/;listen.owner = www-data/' \
+        --expression='s/^listen.group = www-data/;listen.group = www-data/' \
+        --expression='s/^user = www-data/;user = www-data/' \
+        --expression='s/^group = www-data/;group = www-data/' \
+        /etc/php/7.3/fpm/pool.d/www.conf
+# patch /etc/php/7.3/fpm/php-fpm.conf to not have a pidfile
 sed --in-place='' \
-        --expression='s/^pid =/#pid =/' \
-        /etc/php5/fpm/php-fpm.conf
-# patch nginx conf to not bother trying to setuid, since we're not root
-# also patch errors to go to stderr, and logs nowhere.
+        --expression='s/^pid =/;pid =/' \
+        /etc/php/7.3/fpm/php-fpm.conf
+# patch /etc/php/7.3/fpm/php-fpm.conf to place the sock file in /var
 sed --in-place='' \
-        --expression 's/^user www-data/#user www-data/' \
-        --expression 's#^pid /run/nginx.pid#pid /var/run/nginx.pid#' \
-        --expression 's/^\s*error_log.*/error_log stderr;/' \
-        --expression 's/^\s*access_log.*/access_log off;/' \
-        /etc/nginx/nginx.conf
-# Add a conf snippet providing what sandstorm-http-bridge says the protocol is as var fe_https
-cat > /etc/nginx/conf.d/50sandstorm.conf << EOF
-    # Trust the sandstorm-http-bridge's X-Forwarded-Proto.
-    map \$http_x_forwarded_proto \$fe_https {
-        default "";
-        https on;
-    }
-EOF
-# Adjust fastcgi_params to use the patched fe_https
+       --expression='s/^listen = \/run\/php\/php7.3-fpm.sock/listen = \/var\/run\/php\/php7.3-fpm.sock/' \
+        /etc/php/7.3/fpm/pool.d/www.conf
+# patch /etc/php/7.3/fpm/pool.d/www.conf to no clear environment variables
+# so we can pass in SANDSTORM=1 to apps
 sed --in-place='' \
-        --expression 's/^fastcgi_param *HTTPS.*$/fastcgi_param  HTTPS               \$fe_https if_not_empty;/' \
-        /etc/nginx/fastcgi_params
+        --expression='s/^;clear_env = no/clear_env=no/' \
+        /etc/php/7.3/fpm/pool.d/www.conf
