bd8002965caecb953656a4574b503e45ca24c06d
Ian Denhardt <ian@zenhack.net>
2022-03-22 18:26:35 -0400

    Change table name for consistency.

--- SandstormAuth-extension/includes/SandstormAuthHooks.php
+++ SandstormAuth-extension/includes/SandstormAuthHooks.php
@@ -4,7 +4,7 @@
 class SandstormAuthHooks {
 	public static function onLoadExtensionSchemaUpdates(DatabaseUpdater $updater) {
 		$updater->getDB()->query(
-			"CREATE TABLE IF NOT EXISTS sandsorm_users " .
+			"CREATE TABLE IF NOT EXISTS sandsorm_user " .
 			"    ( user_id VARCHAR PRIMARY KEY" .
 			"    , username VARCHAR UNIQUE NOT NULL" .
 			"    , permissions VARCHAR NOT NULL" .
